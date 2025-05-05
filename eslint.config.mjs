21a77f438e127662b321dcb2984762657f8e0be0
Jason Gessner <jason@multiply.org>
2025-05-04 19:38:48 -0500

    Migrate to later eslint version.

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/dist"]), {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            warnOnUnsupportedTypeScriptVersion: false,
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+    },
+},
+{
+    files: ["test/*"],
+    rules: {
+        // Don't throw lint errors on chai assertion style.
+        "@typescript-eslint/no-unused-expressions": "off"
+    }
+}
+]);
